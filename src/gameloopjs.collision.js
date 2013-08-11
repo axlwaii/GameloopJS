8238417eb21df33969293c2187f52497590bd4f4
axlwaii <mwfort-github@yahoo.com>
2013-08-11 13:48:32 +0200

    add new collision module

---
+++ src/gameloopjs.collision.js
@@ -0,0 +1,37 @@
+// Gameloop
+
+GameLoop.collided = function(a, b) {
+
+    //check for horz collision
+    if(b.x + b.width >= a.x && b.x < a.x + a.width) {
+        //check for vert collision
+        if(b.y + b.height >= a.y && b.y < a.y + a.height) {
+            return true;
+        }
+    }
+
+    //check a inside b
+    if(b.x <= a.x && b.x + b.width >= a.x+a.width) {
+        if(b.y <= a.y && b.y + b.height >= a.y + a.height) {
+            return true;
+        }
+    }
+
+    //check b inside a
+    if(a.x <= b.x && a.x + a.width >= b.x+b.width) {
+        if(a.y <= b.y && a.y + a.height >= b.y+b.height) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+GameLoop.dotProduct = function(a, b) {
+  var dx, dy;
+
+  dx = a.x*b.x;
+  dy = a.y*b.y;
+
+  return dx + dy;
+};
