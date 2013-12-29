271242809ed780363141143eef876865d01f3f61
Markus Waitl <markus.waitl@absolventa.de>
2013-12-29 23:17:27 +0100

    Indent Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,24 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-    build: {
-      src: 'src/<%= pkg.name %>.core.js',
-    dest: '<%= pkg.name %>.min.js'
-    }
-    }
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+
+            build: {
+                src: 'src/<%= pkg.name %>.core.js',
+                dest: '<%= pkg.name %>.min.js'
+            }
+        }
     });
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+    // Default task(s).
+    grunt.registerTask('default', ['uglify']);
 
 };
