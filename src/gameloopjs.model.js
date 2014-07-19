d6cb4f89595e5b05f8956717f3f76141d0ef55e8
Markus Waitl <markus.waitl@absolventa.de>
2014-07-19 16:33:29 +0200

    add param z to GameLoop.Model

--- src/gameloopjs.model.js
+++ src/gameloopjs.model.js
@@ -6,14 +6,16 @@
  * @param y y-positon
  * @param h height of the game object
  * @param w width  of the game object
+ * @param z used for rendering order in the gameloop
  */
 
-GameLoop.Model = function(x, y , w, h){
+GameLoop.Model = function(x, y , w, h, z){
 
     'use strict';
 
-    this.x            = x || 0;
+    this.x            = x || 0;
     this.y            = y || 0;
+    this.z            = z || 0;
     this.height       = h || 0;
     this.width        = w || 0;
 
