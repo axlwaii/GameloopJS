c8c4d270c53e13e6f98eb07fc56435e70c5c7ef5
Markus Waitl <markus.waitl@absolventa.de>
2014-07-19 16:32:00 +0200

    improve collision

--- src/gameloopjs.collision.js
+++ src/gameloopjs.collision.js
@@ -1,66 +1,24 @@
 // Gameloop Collisions
 
-window.Gameloop = Gameloop || {};
+window.GameLoop = GameLoop || {};
 
 GameLoop.Collision = (function() {
     'use strict';
 
-    var innerCollision,
-        objectsInnerCollision,
-        dotProduct,
-        collided,
-        intersectRect;
+    var dotProduct,
+        topAreaCollision,
+        bottomAreaCollision,
+        leftAreaCollision,
+        rightAreaCollision,
+        gameAreaCollision,
+        boxCollision,
+        _boxCollision;
 
-    intersectRect = function(r1, r2)  {
+    _boxCollision = function(r1, r2)  {
         return !(r2.left > r1.right ||
             r2.right < r1.left ||
             r2.top > r1.bottom ||
             r2.bottom < r1.top);
-    };
-
-    collided = function(a, b) {
-
-        //check for horz collision
-        if(b.x + b.width >= a.x && b.x < a.x + a.width) {
-            //check for vert collision
-            if(b.y + b.height >= a.y && b.y < a.y + a.height) {
-                return true;
-            }
-        }
-
-        //check a inside b
-        if(b.x <= a.x && b.x + b.width >= a.x+a.width) {
-            if(b.y <= a.y && b.y + b.height >= a.y + a.height) {
-                return true;
-            }
-        }
-
-        //check b inside a
-        if(a.x <= b.x && a.x + a.width >= b.x+b.width) {
-            if(a.y <= b.y && a.y + a.height >= b.y+b.height) {
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-
-    objectsInnerCollision = function(a,b){
-        //check a inside b
-        if(b.x <= a.x && b.x + b.width >= a.x+a.width) {
-            if(b.y <= a.y && b.y + b.height >= a.y + a.height) {
-                return true;
-            }
-        }
-
-        //check b inside a
-        if(a.x <= b.x && a.x + a.width >= b.x+b.width) {
-            if(a.y <= b.y && a.y + a.height >= b.y+b.height) {
-                return true;
-            }
-        }
-        return false;
     };
 
     dotProduct = function(a, b) {
@@ -73,42 +31,100 @@ GameLoop.Collision = (function() {
 
     };
 
-    innerCollision = function(aX, aY, aHeight, aWidth, bX, bY, bHeight, bWidth){
+    bottomAreaCollision = function(y, canvasHeight) {
+
+        if (y >= canvasHeight) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    topAreaCollision = function(y) {
+
+        if ( y <= 0 ) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    leftAreaCollision = function(x) {
+        if (x <= 0 ) {
+            return true;
+        }
+
+        return false;
+    };
+
+    rightAreaCollision = function(x, canvasWidth) {
+
+        if (x >= canvasWidth) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    gameAreaCollision = function(obj, cnvs) {
+
+        if (leftAreaCollision(obj.x) ||
+            rightAreaCollision((obj.x + obj.width),cnvs.width)) {
+            return true;
+        }
+
+        if (topAreaCollision(obj.y)||
+            bottomAreaCollision((obj.y+obj.height),cnvs.height)) {
+            return true;
+        }
+
+        return false;
+
+    };
+
+    // boxCollision = function(aX, aY, aHeight, aWidth, bX, bY, bHeight, bWidth){
+    boxCollision = function(oa, ob){
 
         // var a = {
-        //         x     : aX,
-        //         y     : aY,
-        //         height: aHeight,
-        //         width : aWidth
+        //         left     : aX,
+        //         right    : aX + aWidth,
+        //         top      : aY,
+        //         bottom   : aY + aHeight
         //     },
         //     b = {
-        //         x     : bX,
-        //         y     : bY,
-        //         height: bHeight,
-        //         width : bWidth
+        //         left     : bX,
+        //         right    : bX + bWidth,
+        //         top      : bY,
+        //         bottom   : bY + bHeight
         //     };
 
         var a = {
-                left     : aX,
-                right    : aX + aWidth,
-                top      : aY,
-                bottom   : aY + aHeight
+                left     : oa.x,
+                right    : oa.x + a.width,
+                top      : oa.y,
+                bottom   : oa.y + a.height
             },
             b = {
-                left     : bX,
-                right    : bX + bWidth,
-                top      : bY,
-                bottom   : bY + bHeight
+                left     : ob.x,
+                right    : ob.x + b.width,
+                top      : ob.y,
+                bottom   : ob.y + b.height
             };
 
-        // return objectsInnerCollision(a,b);
-        return intersectRect(a,b);
+        return _boxCollision(a,b);
 
     };
 
     return {
-        dotProduct     : dotProduct,
-        innerCollision : innerCollision
+        dotProduct           : dotProduct,
+        boxCollision         : boxCollision,
+        gameAreaCollision    : gameAreaCollision,
+        topAreaCollision     : topAreaCollision,
+        bottomAreaCollision  : bottomAreaCollision,
+        leftAreaCollision    : leftAreaCollision,
+        rightAreaCollision   : rightAreaCollision
     };
-
-}());
+})();
